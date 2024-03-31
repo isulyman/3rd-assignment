d91af85697f4a88b0ce875298e78505f1c609e21
isulyman <isulymanali@gmail.com>
2024-03-31 15:12:34 +0500

    initial comit

---
+++ index.ts
@@ -0,0 +1,26 @@
+let movieName: string = "Hulk";
+let myname: string = "Sulyman";
+let myage: number = 41;
+let intro = `my name is ${myname} and my age is ${myage}`;
+const myMood: Boolean = true;
+let fName: string = "Salman";
+let lName: string = "Ali";
+let fullName = `${fName} ${lName}`;
+console.log(fullName);
+let userAge: number = 52;
+let productName: string = "Table";
+let productPrice: number = 5200;
+let productDetail = `Product Name is ${productName} and price is Rs.${productPrice}.`;
+console.log(productDetail);
+let numberofPet: number = 21;
+let todayisbirthday: Boolean = true;
+
+let firstName: string = "John";
+let lastName: string = "Doe";
+let age: number = 30;
+
+let userInfo: string = `User Information: First Name: ${firstName} Last Name: ${lastName} Age: ${age}`;
+
+// let userinfo2: string = `user information: FirstName ${firstName}`;
+
+console.log(userInfo);
